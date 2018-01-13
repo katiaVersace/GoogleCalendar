905ffa70a8218cd284f051464f579f86d273821a
Marco Amato <amatomarco1@gmail.com>
2018-01-13 02:11:16 +0100

    login/registration migliorate

--- GoogleCalendar/WebContent/WEB-INF/resources/scripts/login.js
+++ GoogleCalendar/WebContent/WEB-INF/resources/scripts/login.js
@@ -1,12 +1,6 @@
 $(function() {
 
-    $('#login-form-link').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
+    
 	$('#register-form-link').click(function(e) {
 		$("#register-form").delay(100).fadeIn(100);
  		$("#login-form").fadeOut(100);
@@ -15,4 +9,11 @@ $(function() {
 		e.preventDefault();
 	});
 
+	 $('#login-form-link').click(function(e) {
+			$("#login-form").delay(100).fadeIn(100);
+	 		$("#register-form").fadeOut(100);
+			$('#register-form-link').removeClass('active');
+			$(this).addClass('active');
+			e.preventDefault();
+		});
 });
