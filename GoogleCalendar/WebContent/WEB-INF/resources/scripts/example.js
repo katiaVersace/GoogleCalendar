b467f5170d0edd2d7bd45130d990b1d52958e444
Giuseppe Benvenuto <gius.benvenuto@gmail.com>
2018-01-14 17:53:36 +0100

    nuovo branch ihihihihihihihih

--- GoogleCalendar/WebContent/WEB-INF/resources/scripts/example.js
+++ GoogleCalendar/WebContent/WEB-INF/resources/scripts/example.js
@@ -1,121 +1,129 @@
-var test = [
-	{
-        title: 'An event',
- //     color: calendarConfig.colorTypes.warning,
-        startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
-        endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
-        draggable: true,
-        resizable: true,
-      //actions: actions
-      },
-];
+var edb = {};
 
 angular.module('mwl.calendar.docs', ['mwl.calendar', 'ngAnimate', 'ui.bootstrap', 'colorpicker.module']);
-angular
-  .module('mwl.calendar.docs') //you will need to declare your module with the dependencies ['mwl.calendar', 'ui.bootstrap', 'ngAnimate']
-  .controller('KitchenSinkCtrl', function(moment, alert, calendarConfig) {
-
+angular.module('mwl.calendar.docs') .controller('KitchenSinkCtrl', function(moment, calendarConfig) {     
     var vm = this;
-
-    //These variables MUST be set as a minimum for the calendar to work
+    
     vm.calendarView = 'month';
     vm.viewDate = new Date();
-    var actions = [{
-      label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
-      onClick: function(args) {
-        alert.show('Edited', args.calendarEvent);
-      }
-    }, {
-      label: '<i class=\'glyphicon glyphicon-remove\'></i>',
-      onClick: function(args) {
-        alert.show('Deleted', args.calendarEvent);
-      }
-    }];
-    vm.events = test/*[
-      {
-        title: 'An event',
-        color: calendarConfig.colorTypes.warning,
-        startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
-        endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
-        draggable: true,
-        resizable: true,
-        actions: actions
-      }, {
-        title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
-        color: calendarConfig.colorTypes.info,
-        startsAt: moment().subtract(1, 'day').toDate(),
-        endsAt: moment().add(5, 'days').toDate(),
-        draggable: true,
-        resizable: true,
-        actions: actions
-      }, {
-        title: 'This is a really long event title that occurs on every year',
-        color: calendarConfig.colorTypes.important,
-        startsAt: moment().startOf('day').add(7, 'hours').toDate(),
-        endsAt: moment().startOf('day').add(19, 'hours').toDate(),
-        recursOn: 'year',
-        draggable: true,
-        resizable: true,
-        actions: actions
-      }
-    ];*/;
+
+    var actions = [
+        {
+            label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+            onClick: function(args) {
+                vm.deleteEvent(args.calendarEvent);
+          }
+        },
+    ];
+
+    vm.events = [];
+    
+    vm.addEvent = function() {
+        vm.events.push({
+            title: 'New event',
+            startsAt: moment().startOf('day').toDate(),
+            endsAt: moment().endOf('day').toDate(),
+            color: calendarConfig.colorTypes.important,
+            draggable: false,
+            resizable: false,
+            actions: actions
+        });
+    };
+    
+    vm.deleteEvent = function (event) {
+        vm.events = vm.events.filter(function (item) {
+            return item.id != event.id;
+        });
+        
+        edb[event.calendar].events = edb[event.calendar].events.filter(function (item) {
+            return item.id != event.id;
+        });
+    };
+    
+    vm.shownCalendars = [];
+    vm.checkedCalendars = [];
+    
+    vm.showCalendar = function (id) {            
+        if (edb.hasOwnProperty(id) && !vm.shownCalendars.includes(id)) {
+            for (var i = 0; i < edb[id]["events"].length; i++) {
+                var event;
+                event = edb[id]["events"][i];
+                event.calendar = id;
+                event.startsAt = new Date(event.startsAt);
+                event.endsAt = new Date(event.endsAt);
+                event.actions = actions;
+                event.draggable = false;
+                event.resizable = false;
+                
+                vm.events.push(event);
+            }
+            vm.shownCalendars.push(id);
+        }
+    };
+    
+    vm.hideCalendar = function (id) {
+        if (vm.shownCalendars.includes(id)) {
+            var i = vm.events.length;
+            while (i--) {
+                if (vm.events[i].calendar == id) {
+                    vm.events.splice(i, 1);
+                }
+            }
+            
+            var index = vm.shownCalendars.indexOf(id);
+            if (index !== -1) {
+                vm.shownCalendars.splice(index, 1);
+            }
+        }
+    };
+    
+    vm.toggleCalendar = function (id) {
+        if (vm.checkedCalendars[id]) {
+            vm.showCalendar(id);
+        } else {
+            vm.hideCalendar(id);
+        }
+    };
 
     vm.cellIsOpen = true;
 
-    vm.addEvent = function() {
-      vm.events.push({
-        title: 'New event',
-        startsAt: moment().startOf('day').toDate(),
-        endsAt: moment().endOf('day').toDate(),
-        color: calendarConfig.colorTypes.important,
-        draggable: false,
-        resizable: false,
-        actions: actions
-      });
-    };
-
     vm.eventClicked = function(event) {
-      alert.show('Clicked', event);
+        // TODO
     };
 
     vm.eventEdited = function(event) {
-      alert.show('Edited', event);
+        // TODO
     };
 
     vm.eventDeleted = function(event) {
-      alert.show('Deleted', event);
+        // TODO
     };
 
     vm.eventTimesChanged = function(event) {
-      alert.show('Dropped or resized', event);
+        // TODO
     };
 
     vm.toggle = function($event, field, event) {
-      $event.preventDefault();
-      $event.stopPropagation();
-      event[field] = !event[field];
+        $event.preventDefault();
+        $event.stopPropagation();
+        event[field] = !event[field];
     };
 
-    vm.timespanClicked = function(date, cell) {
-
-      if (vm.calendarView === 'month') {
-        if ((vm.cellIsOpen && moment(date).startOf('day').isSame(moment(vm.viewDate).startOf('day'))) || cell.events.length === 0 || !cell.inMonth) {
-          vm.cellIsOpen = false;
-        } else {
-          vm.cellIsOpen = true;
-          vm.viewDate = date;
-        }
-      } else if (vm.calendarView === 'year') {
-        if ((vm.cellIsOpen && moment(date).startOf('month').isSame(moment(vm.viewDate).startOf('month'))) || cell.events.length === 0) {
-          vm.cellIsOpen = false;
-        } else {
-          vm.cellIsOpen = true;
-          vm.viewDate = date;
-        }
-      }
-
+    vm.timespanClicked = function(date, cell) { 
+        if (vm.calendarView === 'month') {
+            if ((vm.cellIsOpen && moment(date).startOf('day').isSame(moment(vm.viewDate).startOf('day'))) || cell.events.length === 0 || !cell.inMonth) {
+                vm.cellIsOpen = false;
+            } else {
+                vm.cellIsOpen = true;
+                vm.viewDate = date;
+            }
+        } else if (vm.calendarView === 'year') {
+            if ((vm.cellIsOpen && moment(date).startOf('month').isSame(moment(vm.viewDate).startOf('month'))) || cell.events.length === 0) {
+                vm.cellIsOpen = false;
+            } else {
+                vm.cellIsOpen = true;
+                vm.viewDate = date;
+            }
+        }    
     };
-
-  });
-
-
+});
--- GoogleCalendar/src/it/unical/googlecalendar/controllers/IndexController.java
+++ GoogleCalendar/src/it/unical/googlecalendar/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package it.unical.googlecalendar.controllers;
 
+import java.util.Collection;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.google.gson.*;
+
+import it.unical.googlecalendar.model.Calendar;
 import it.unical.googlecalendar.services.DbService;
 
 @Controller
@@ -18,9 +23,14 @@ public class IndexController {
 	@RequestMapping("/index")
 	public String homePage(Model model, HttpSession session) {
 		String email=(String) session.getAttribute("email");
-	if(email==null)	return "redirect:/";
+		
+		if(email==null)	return "redirect:/";
+		
+		Collection<Calendar> collectionCalendar = dbService.getCalendarsForUser(email);
+		
 		model.addAttribute("events", dbService.stampaEventiPerUtente(email));
-		model.addAttribute("calendars", dbService.getCalendarsForUser(email));
+		model.addAttribute("calendars", collectionCalendar);
+		model.addAttribute("test", (new Gson()).toJson(collectionCalendar));
 
 		return "index";
 	}
